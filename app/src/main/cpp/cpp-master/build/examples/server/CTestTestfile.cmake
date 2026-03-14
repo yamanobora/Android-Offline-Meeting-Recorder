9127c3ad5a296579dd29466344145942d8c08ffa
yamanobora <muranotayori@gmail.com>
2026-03-15 00:16:18 +0900

    Add files via upload

---
+++ app/src/main/cpp/cpp-master/build/examples/server/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Desktop/whisper.cpp-master/examples/server
+# Build directory: C:/Users/user/Desktop/whisper.cpp-master/build/examples/server
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ app/src/main/cpp/cpp-master/build/examples/vad-speech-segments/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Desktop/whisper.cpp-master/examples/vad-speech-segments
+# Build directory: C:/Users/user/Desktop/whisper.cpp-master/build/examples/vad-speech-segments
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
